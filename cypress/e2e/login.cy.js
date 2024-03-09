164a4e0bad24dd647989f6102f170e332b857840
Alexander Belchev <randomryze1@gmail.com>
2024-03-09 10:41:36 +0200

    Initial commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,27 +1,10 @@
-describe('Registration tests', () => {
+describe('Login tests', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:8080/register')
-  })
-
-  it('enter invalid and then enter valid username',() => {
-   cy.get('input[type="submit"]')
-       .click();
-
-    cy.get('[data-test-id="fullname-error"]')
-        .should('be.visible');
-
-    cy.get('input[type="submit"]')
-        .click();
-    cy.get('#fullname')
-        .type("John Doe");
-    cy.get('input[type="submit"]')
-        .click();
-    cy.get('[data-test-id="fullname-error"]')
-        .should('not.exist');
+    cy.visit('http://localhost:8080/login')
   })
 
   it('enter invalid and then enter valid email', () => {
@@ -55,43 +38,11 @@ describe('Registration tests', () => {
         .should('not.exist');
   });
 
-  it('enter invalid and then enter valid date of birth', () => {
-    cy.get('input[type="submit"]')
-       .click();
-    cy.get('[data-test-id="date-error"]')
-       .should('be.visible');
-    cy.get('#date')
-       .type('2024-01-01');
-    cy.get('input[type="submit"]')
-       .click();
-    cy.get('[data-test-id="date-error"]')
-       .should('not.exist');
-  });
-
-  it('enter invalid and then enter valid gender', () => {
-   cy.get('input[type="submit"]')
-      .click();
-   cy.get('[data-test-id="gender-error"]')
-      .should('be.visible');
-   cy.get('#gender')
-      .select('Male');
-   cy.get('input[type="submit"]')
-      .click();
-   cy.get('[data-test-id="gender-error"]')
-      .should('not.exist');
-  });
-
   it('enter valid data and click submit',()=> {
-    cy.get('#fullname')
-        .type("John Doe");
     cy.get('#email')
         .type("john_does@email.com");
     cy.get('#password')
         .type("12345678");
-    cy.get('#gender')
-        .select("Male");
-    cy.get('#date')
-        .type("2000-01-01");
     cy.get('input[type="submit"]')
         .click();
     cy.url()
