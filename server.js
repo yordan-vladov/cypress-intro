166f828a67bb44379b0632fd88233f4afa095d76
As0fin <125550073+As0fin@users.noreply.github.com>
2024-03-14 22:20:37 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
-// sendFile will go here
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname, '/register.html'));
+});
 
-  app.get('/thank-you', function(req, res) {
+app.get('/login', function(req, res) {
+   res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+app.get('/thank-you', function(req, res){
     res.sendFile(path.join(__dirname, '/thank-you.html'));
-  });
-  
-  
+});
+ 
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
