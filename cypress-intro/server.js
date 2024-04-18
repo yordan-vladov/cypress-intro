e9aa4d228fe4fc0c506b1f61ace083e49c68e7bb
g2brim <157985888+g2brim@users.noreply.github.com>
2024-04-18 10:47:58 +0300

    Add files via upload

---
+++ cypress-intro/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+
+  app.get('/thank-you', function(req, res) {
+app.get('/register', function(req, res) {
+  res.sendFile(path.join(__dirname, '/register.html'));
+});
+
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+app.get('/thank-you', function(req, res) {
+    res.sendFile(path.join(__dirname, '/thank-you.html'));
+  });
+});
+
+
+
+
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
